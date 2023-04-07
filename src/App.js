d1eaa0d87374e2d0ae0893472f2a6ab70b0a0576
ramesh <ramesh@RFL424.local>
2023-04-07 15:06:49 +0530

    Completed the routing concept

--- src/App.js
+++ src/App.js
@@ -3,58 +3,56 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./login";
 import { Register } from "./Register";
 import { Home } from "./home";
-import { Navigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
 import { auth } from "./firebase";
-import { Outlet } from "react-router-dom";
 import { Profile } from "./profile";
 import { PrivateRoute } from "./privateRoute";
+import { useEffect, useState } from "react";
+
 function App() {
-  const [authenticated, setAuthenticated] = useState();
+  const [check, setCheck] = useState();
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const localValue = localStorage.setItem("signed in", "true");
+    } else {
+      localStorage.removeItem("signed in");
+    }
+  });
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setAuthenticated(user);
+      setCheck(user);
     });
   }, []);
-  console.log("current user", authenticated);
   return (
     <>
-      {authenticated ? (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Login />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+
+          {check || localStorage.getItem("signed in") ? (
             <Route path="/home" element={<Home />}></Route>
-            <Route
-              path="/home"
-              element={
-                <>
-                  <PrivateRoute isLogin={authenticated}>
-                    <Home />
-                  </PrivateRoute>
-                </>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <PrivateRoute isLogin={authenticated}>
-                  <Profile />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      ) : (
-        <BrowserRouter>
-          <Routes>
+          ) : (
+            <Route path="/home" element={<Login />}></Route>
+          )}
+
+          {localStorage.getItem("signed in") ? (
+            <Route path="/" element={<Home />}></Route>
+          ) : (
             <Route path="/" element={<Login />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-          </Routes>
-        </BrowserRouter>
-      )}
+          )}
+
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute isLogin={localStorage.getItem("signed in")}>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
